43b5871e167b48b0fe6e737714604cf4295d4db0
Yazir Solis <yazir.solis@gmail.com>
2020-10-14 14:05:00 -0500

    first commit

---
+++ Assets/Scripts/CambiarColorEmision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambiarColorEmision : MonoBehaviour
+{
+    public Material matOriginal;
+    public Material matCambio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CambiarColor());
+    }
+
+    IEnumerator CambiarColor()
+    {
+        yield return new WaitForSeconds(5f);
+
+        this.GetComponent<Renderer>().material = matCambio;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
